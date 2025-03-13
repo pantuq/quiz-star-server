9cbfc7fd3ebeb34fd4d2aed5b4d2ba2db5a45132
tuq <tuq_5986@163.com>
2025-03-13 20:50:43 +0800

    feat: 使用interceptor和filter统一路由返回格式

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './transform/transform.interceptor';
+import { HttpExceptionFilter } from './http-execption/http-execption.filter';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.setGlobalPrefix('api'); // 路由设置全局前缀
+    app.useGlobalInterceptors(new TransformInterceptor()); // 全局拦截器--返回数据格式化
+    app.useGlobalFilters(new HttpExceptionFilter()); //全局拦截器--异常过滤器
     await app.listen(process.env.PORT ?? 3005);
 }
 bootstrap();
