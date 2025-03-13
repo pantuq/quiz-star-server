3648218e84f2e14381b1e191fe6849972cd8a9ee
tuq <tuq_5986@163.com>
2025-03-13 20:32:22 +0800

    test: 路由常见功能demo

---
+++ src/question/dto/question.dto.ts
@@ -0,0 +1,5 @@
+//  dto - data transfer object 数据传输对象
+export class QuestionDto {
+    readonly title: string;
+    readonly desc: string;
+}
--- src/question/question.controller.ts
+++ src/question/question.controller.ts
@@ -1,17 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Query } from '@nestjs/common';
+import { QuestionDto } from './dto/question.dto';
 
 @Controller('question')
 export class QuestionController {
     @Get() // GET /question 就是get请求
-    findAll() {
+    findAll(
+        @Query('keyword') keyword: string,
+        @Query('page') page: number,
+        @Query('pageSize') pageSize: number,
+    ) {
+        console.log(keyword, page, pageSize);
         return {
             list: ['a', 'b', 'c'],
             count: 10,
         };
     }
 
-    @Get('test')
-    getTest(): string {
-        return 'question test';
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return {
+            id,
+            title: 'test',
+            content: 'test content',
+        };
+    }
+
+    @Patch(':id')
+    updateOne(@Param('id') id: string, @Body() questionDto: QuestionDto) {
+        console.log('questionDto', questionDto);
+        return {
+            id,
+            title: 'aaa',
+            content: 'bbb',
+        };
     }
 }
