bf8855533d65d8086c15b6cc5f77186b78a0e43d
tuq <tuq_5986@163.com>
2025-03-14 21:45:46 +0800

    feat: 使用service操作数据库，创建问卷和查找问卷

--- src/question/question.controller.ts
+++ src/question/question.controller.ts
@@ -1,8 +1,26 @@
-import { Body, Controller, Get, Param, Patch, Query } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import {
+    Body,
+    Controller,
+    Get,
+    Param,
+    Patch,
+    Post,
+    Query,
+} from '@nestjs/common';
 import { QuestionDto } from './dto/question.dto';
+import { QuestionService } from './question.service';
 
 @Controller('question')
 export class QuestionController {
+    // 依赖注入
+    constructor(private readonly questionService: QuestionService) {}
+
+    @Post()
+    create() {
+        return this.questionService.create();
+    }
+
     @Get() // GET /question 就是get请求
     findAll(
         @Query('keyword') keyword: string,
@@ -18,11 +36,7 @@ export class QuestionController {
 
     @Get(':id')
     findOne(@Param('id') id: string) {
-        return {
-            id,
-            title: 'test',
-            content: 'test content',
-        };
+        return this.questionService.findOne(id);
     }
 
     @Patch(':id')
--- src/question/question.service.ts
+++ src/question/question.service.ts
@@ -1,4 +1,28 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Question } from './schemas/question.schema';
 
 @Injectable()
-export class QuestionService {}
+export class QuestionService {
+    constructor(
+        // 依赖注入
+        @InjectModel(Question.name) private readonly questionModel,
+    ) {}
+
+    async create() {
+        const question = new this.questionModel({
+            title: 'title' + Date.now(),
+            desc: 'desc',
+        });
+
+        return await question.save();
+    }
+
+    async findOne(id: string) {
+        return await this.questionModel.findById(id);
+    }
+}
