20d2f430d57dae984f006e9f9c03e8bba7e74f8d
tuq <tuq_5986@163.com>
2025-03-14 21:03:16 +0800

    feat: 使用nextjs连接mongodb数据库

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuestionModule } from './question/question.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [QuestionModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        QuestionModule,
+        MongooseModule.forRoot('mongodb://127.0.0.1:27017/nestdb'),
+        ConfigModule.forRoot(),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
