ee7fdd42eb52a18c4bb6b6fe3a9271dfe81a8ff5
tuq <tuq_5986@163.com>
2025-03-18 20:09:09 +0800

    feat: 开发单个组件的统计功能

--- src/stat/stat.service.ts
+++ src/stat/stat.service.ts
@@ -105,4 +105,58 @@ export class StatService {
             total,
         };
     }
+
+    // 获取单个组件的统计数据
+    async getComponentStat(questionId: string, componentFeId: string) {
+        if (!questionId || !componentFeId) return [];
+
+        // 获取问卷
+        const q = await this.questionService.findOne(questionId); //问卷
+        if (q == null) return [];
+
+        // 获取组件
+        const { componentList = [] } = q;
+        const comp = componentList.filter((c) => c.feId === componentFeId)[0];
+        if (comp == null) return [];
+
+        const { type, props } = comp;
+        if (type !== 'questionRadio' && type !== 'questionCheckbox') {
+            // 单组件的，只能统计单选和多选,其他不统计
+            return [];
+        }
+
+        // 获取答卷列表
+        const total = await this.answerService.count(questionId);
+        if (total === 0) return []; //答卷总数量
+        const answers = await this.answerService.findAll(questionId, {
+            page: 1,
+            pageSize: total,
+        });
+
+        // 累加各个value数量
+        const countInfo = {};
+        answers.forEach((a) => {
+            if (a.componentFeId !== componentFeId) return;
+            a.value.forEach((v) => {
+                if (countInfo[v] == null) countInfo[v] = 0;
+                countInfo[v]++;
+            });
+        });
+
+        // 整理数据
+        const list: { name: string; count: number }[] = [];
+        for (const val in countInfo) {
+            // 根据val计算text
+            let text = '';
+            if (type === 'questionRadio') {
+                text = this._getRadioOptText(val, props);
+            }
+            if (type === 'questionCheckbox') {
+                text = this._getCheckboxOptText(val, props);
+            }
+
+            list.push({ name: text, count: countInfo[val] });
+        }
+        return list;
+    }
 }
